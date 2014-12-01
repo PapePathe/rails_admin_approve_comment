4376d03250edee069ae1ea134ad7d29c8d68bc42
Pathe SENE <pathe.sene@gmail.com>
2014-12-01 10:48:00 +0000

    Add a cusom icon & activate the button only for members.

--- lib/rails_admin_approve_event.rb
+++ lib/rails_admin_approve_event.rb
@@ -10,7 +10,18 @@ module RailsAdmin
     module Actions
       class ApproveEvent < Base
         RailsAdmin::Config::Actions.register(self)
-        
+
+        register_instance_option :link_icon do
+          'icon-check'
+        end
+        register_instance_option :visible? do
+          authorized? && bindings[:object].class == Evenement
+        end
+
+        register_instance_option :member? do
+          true
+        end
+
         register_instance_option :object_level do
           true
         end
